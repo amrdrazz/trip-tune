1ff09788d4898c14a66156ab48f5280ab28fc658
amr <amr.draz.2009@gmail.com>
2026-05-11 17:10:54 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,55 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+app.use(express.json());
+
+const Place = require("./models/place");
+
+mongoose.connect(process.env.database)
+.then(()=>{
+    console.log("Connected successfuly");
+})
+.catch((error)=>{
+    console.log("error with connecting database/ " + error);
+});
+
+
+
+
+//  Endpoints =========
+
+
+app.get("/getplaces", async (req, res)=>{
+    const places = await Place.find();
+    res.send(places)
+});
+
+app.get("/getplaces/:class", async (req, res)=>{
+    const places = await Place.find({
+        vibe: req.params.class
+    });
+    res.send(places)
+});
+
+app.get("/getplaces/:class/:city", async (req, res)=>{
+    let places = []
+    if (req.params.class === 'any'){
+        places = await Place.find({
+            government: req.params.city
+        });
+    }else{
+        places = await Place.find({
+            vibe: req.params.class,
+            government: req.params.city
+        });
+    }
+    res.send(places)
+});
+
+
+// =============
+app.listen(3000, ()=>{
+    console.log("I am listening in port 3000");
+})
---
+++ models/place.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const placeSchema = new Schema({
+    title: String,
+    about: String,
+    images: {
+        head: String,         //the main image
+        allImages: Array      //the swiper images
+    },
+    video: String,
+    openingHours: String,    
+    location: String,
+    government: String,
+    price: String,
+    vibe: [String],            //[creative,social,organized,analytical]
+    ticket: String,
+    tags: [String],
+    rating: {
+        rate: Number,
+        usersRated: Number
+    }
+});
+
+const Place = mongoose.model("Place", placeSchema);
+
+module.exports = Place;
